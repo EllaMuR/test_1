5715c7eed9f611e4e50fa3620f8bd5a76a02c10d
ella.muhametshina <ella.muhametshina@lenvendo.ru>
2024-04-08 17:55:10 +0300

    добавлена проверка на активацию кнопки

--- fixture/card_noms.py
+++ fixture/card_noms.py
@@ -6,31 +6,31 @@ class Card_Noms:
         self.app = app
 
     def find_card_noms_quantity(self):
+        #количество кнопок с номиналом
         wd = self.app.wd
         return len(wd.find_elements_by_class_name("par-options__button"))
 
 
     def click_on_card_noms(self, index):
         wd = self.app.wd
-        wd.execute_script("window.scrollBy(0, 800)")
         wd.find_element_by_xpath("//li[%s]/button" %index).click()
         time.sleep(2)
 
 
     def get_button_value(self, index):
+        #получение значения кнопки
         self.click_on_card_noms(index)
-        # c = self.is_button_active()
-        # print(str(c))
         wd = self.app.wd
         return wd.find_element_by_xpath("//li[%s]/button" %index).text
 
     def get_nominal_value(self):
+        #получение значения поля ввода
         wd = self.app.wd
         return wd.find_element_by_id("range-value-input").get_attribute('value')
 
 
-    # def is_button_active(self):
-    #     wd = self.app.wd
-    #     res = wd.find_elements_by_class_name("button--active").get_attribute('value')
-    #     return res
-    #     #assert res == self.get_button_value(index)
+    def is_button_active(self):
+        #проверка на значение активной кнопки
+        wd = self.app.wd
+        res = (wd.find_element_by_class_name("par-options__button--active").text)
+        return res
--- fixture/session.py
+++ fixture/session.py
@@ -1,9 +1,5 @@
 import time
-import requests
-from requests.auth import HTTPDigestAuth
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 
 class SessionHelper:
@@ -16,52 +12,4 @@ class SessionHelper:
         wd = self.app.wd
         wd.get("http://"+username+":"+password+"@"+base_url)
         time.sleep(2)
-        # self.app.open_homepage()
-
-        # WebDriverWait(wd, 10).until(EC.alert_is_present())
-        # username=self.app.config['login_info']['username']
-        # password=self.app.config['login_info']['password']
-        # wd.switch_to.alert.send_keys("HR"+Keys.TAB+"test")
-        # wd.switch_to.alert.accept()
-        # requests.get("https://qa.digift.ru/", auth=HTTPDigestAuth("HR", "test"))
-        # wd.find_element_by_name("username").click()
-        # wd.find_element_by_name("username").clear()
-        # wd.find_element_by_name("username").send_keys(username)
-        # wd.find_element_by_name("password").click()
-        # wd.find_element_by_name("password").clear()
-        # wd.find_element_by_name("password").send_keys(password)
-        # wd.find_element_by_css_selector('input[type="submit"]').click()
-
-
-    # def logout(self):
-    #     wd = self.app.wd
-    #     time.sleep(1)
-    #     wd.find_element_by_link_text("Logout").click()
-    #     time.sleep(1)
-
-    # def ensure_logout(self):
-    #     wd = self.app.wd
-    #     if self.is_logged_in():
-    #         self.logout()
-
-    # def is_logged_in(self):
-    #     wd = self.app.wd
-    #     return len(wd.find_elements_by_link_text("Logout")) > 0
-
-    # def is_logged_in_as(self, username):
-    #     wd = self.app.wd
-    #     return self.get_logged_user() == username
-
-    # def get_logged_user(self):
-    #     wd = self.app.wd
-    #     return wd.find_element_by_css_selector("td.login-info-left span").text
-
-
-    # def ensure_login(self, username, password):
-    #     wd = self.app.wd
-    #     if self.is_logged_in():
-    #         if self.is_logged_in_as(username):
-    #             return
-    #         else:
-    #             self.logout()
-    #     self.login(username, password)
+        wd.execute_script("window.scrollBy(0, 800)")
